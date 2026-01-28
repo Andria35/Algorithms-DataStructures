e718ea2a8070b4c941b49491ab819d6e1022bc35
Andria Inasaridze <andria.inas@gmail.com>
2026-01-28 19:36:53 +0100

    Add two sum algorithm

--- src/arrays/array_problems.cpp
+++ src/arrays/array_problems.cpp
@@ -1,3 +1,39 @@
 #include <iostream>
 #include <vector>
+#include "array.hpp"
+#include <unordered_map>
 
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+*/
+std::vector<int> two_sum(const std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> hash_table;
+    std::vector<int> result;
+
+    for(size_t i = 0; i < nums.size(); ++i) {
+        int a = nums[i];
+        int b = target - a;
+
+        auto element = hash_table.find(b);
+        if (element != hash_table.end()) {
+            result.push_back(element->second);
+            result.push_back(static_cast<int>(i));
+            break;
+        } else {
+            hash_table[a] = static_cast<int>(i);
+        }
+    }
+
+    return result;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include "arrays/array.hpp"
 
+void print_vector(const std::vector<int>& v) {
+    std::cout << std:: endl << "[ ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        std::cout << v[i];
+        if (i + 1 < v.size()) std::cout << ", ";
+    }
+    std::cout << " ]" << std:: endl;
+}
 
 int main() {
-    std::cout << "Hello worldsssssss" << std::endl;
+
+    std::vector<int> vector_two_sum = {3, 3};
+    int target = 6;
+
+    std::vector<int> two_sum_result = two_sum(vector_two_sum, target);
+    print_vector(two_sum_result);
     return 0;
 }
