774c0476cea05ec471dcdf83d949415a4d66ef14
Andria Inasaridze <andria.inas@gmail.com>
2026-01-29 22:43:13 +0100

    update array header

--- src/arrays/array.hpp
+++ src/arrays/array.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 
 std::vector<int> two_sum(const std::vector<int>& nums, int target);
+string longestCommonPrefix(vector<string>& strs);
